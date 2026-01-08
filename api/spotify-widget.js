e2a81bf53d9cfa84af30fb58600504d3c8cd0697
EnrinJr <nrndbrma2@gmail.com>
2026-01-07 19:17:59 -0500

    bro

--- api/spotify-widget.js
+++ api/spotify-widget.js
@@ -4,6 +4,16 @@ export default async function handler(req, res) {
   res.setHeader('Cache-Control', 'no-cache, no-store, must-revalidate');
   res.setHeader('Pragma', 'no-cache');
   res.setHeader('Expires', '0');
+  
+  // Add CORS headers
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+
+  if (req.method === 'OPTIONS') {
+    res.status(200).end();
+    return;
+  }
 
   try {
     const clientId = process.env.SPOTIFY_CLIENT_ID;
