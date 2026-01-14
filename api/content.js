db974f44171d773a8566986ad824b4a8f5acb3df
EnrinJr <nrndbrma2@gmail.com>
2026-01-14 03:55:24 -0500

    update

--- api/content.js
+++ api/content.js
@@ -14,8 +14,8 @@ const TIMELINE_KEY = 'timeline';
 
 // Create Redis client
 const redis = new Redis({
-    url: process.env.UPSTASH_REDIS_REST_URL,
-    token: process.env.UPSTASH_REDIS_REST_TOKEN,
+    url: process.env.KV_REST_API_URL,
+    token: process.env.KV_REST_API_TOKEN,
 });
 
 // Load data from Redis
